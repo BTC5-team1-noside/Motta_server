c28666f36807d69214a3adf1e83090599ebb3f27
YokotaYusuke <tarut8166@gmail.com>
2023-12-21 16:33:17 +0900

    エンドポイント修正2

--- server/src/server.js
+++ server/src/server.js
@@ -4,17 +4,9 @@ const cors = require('cors');
 const setUpServer = () => {
   const app = express();
   app.use(express.json());
+  app.use(cors()); // 全てのアクセスを許可
 
-  app.use(cors());
-
-  // app.use(
-  //   cors({
-  //     origin: 'http://localhost:5173',
-  //     credentials: true,
-  //   })
-  // );
   // app.use(express.urlencoded({ extended: true }));
-
   //   app.use(
   //     express.static(path.join(__dirname, '../../client/team2-client/dist'))
   //   );
